a1deb13446985cf61d67dc9c75b80043fe778214
andruixxd31 <andruix93@gmail.com>
2022-05-28 17:09:04 -0500

    Initial Commit

---
+++ bookKeeper/bookKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookKeeperApp.swift
+//  bookKeeper
+//
+//  Created by Andres on 28/05/22.
+//
+
+import SwiftUI
+
+@main
+struct bookKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

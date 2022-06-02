743a8c64cb71e77030722deff6f8bde8c0f7506a
andruixxd31 <andruix93@gmail.com>
2022-06-01 21:58:35 -0500

    Pantalla de inicio y firestorage integrado

---
+++ bookKeeper/Model/BookUser.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct BookUser {
+    let uid, email, profileImageUrl: String
+    
+    init(data: [String: Any]) {
+        self.uid = data["uid"] as? String ?? ""
+        self.email = data["email"] as? String ?? ""
+        self.profileImageUrl = data["profileImageUrl"] as? String ?? ""
+    }
+}

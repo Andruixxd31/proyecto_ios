9961a2170ae96c0c08c2e7399c7a17e331f1bee6
A01114713 <42302269+A01114713@users.noreply.github.com>
2022-06-15 21:22:48 -0500

    App

---
+++ bookKeeperApp/bookKeeperApp/Utils/FireBaseManager.swift
@@ -0,0 +1,30 @@
+//
+//  FireBaseManager.swift
+//  bookKeeperApp
+//
+//  Created by gdaalumno on 14/06/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseStorage
+
+class FirebaseManager: NSObject {
+    
+    let auth: Auth
+    let storage: Storage
+    let firestore: Firestore
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        if FirebaseApp.app() == nil { FirebaseApp.configure() }
+        
+        self.auth = Auth.auth()
+        self.storage = Storage.storage()
+        self.firestore = Firestore.firestore()
+        
+        super.init()
+    }
+}
